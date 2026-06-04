97965f5c709226a49f4370a9fbc002a9444ccb88
Ryan Spice <spice.ryan@hotmail.com>
2026-06-04 11:37:36 -0400

    Include research library in sitemap

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -34,6 +34,11 @@ export const GET = ({ url }: { url: URL }) => {
 			priority: 1.0
 		},
 		{
+			loc: new URL(`${base}/library/`, url.origin).toString(),
+			changefreq: 'monthly',
+			priority: 0.6
+		},
+		{
 			loc: new URL(`${base}/dev-log/`, url.origin).toString(),
 			changefreq: 'weekly',
 			priority: 0.6
